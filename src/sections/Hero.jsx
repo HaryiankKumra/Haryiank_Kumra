16a1d932a245736b8fc86ea643cc18d36a65cf43
Haryiank Kumra <144940685+HaryiankKumra@users.noreply.github.com>
2026-01-28 09:36:14 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Haryiank, a CS Undergrad student at TIET Patial.
+              Hi, I’m Haryiank, a CS Undergrad student at TIET Patiala.
             </p>
 
             <Button
@@ -76,3 +76,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
